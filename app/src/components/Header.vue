31467404742c5a45f30a5a8a3e8d29dc799fbd53
xxf0007 <92376068+xiaxiao0007@users.noreply.github.com>
2023-03-06 19:57:01 +0800

    components

---
+++ app/src/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <el-header style="border-bottom: 1px solid #ccc; line-height: 60px; display: flex" >
+    <div style="flex: 1;">
+      <span :class="collapseBtnClass" style="cursor: pointer;font-size: 18px" @click="collapse"></span>
+      <el-breadcrumb separator="/" style="display: inline-block;margin-left: 10px;">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>{{$route.meta.name}}</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+
+    <el-dropdown style="width: 70px; cursor: pointer">
+      <span>王小虎</span><i class="el-icon-arrow-down" style="margin-left: 5px"></i>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item>个人信息</el-dropdown-item>
+        <el-dropdown-item>退出</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </el-header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data(){
+    return{
+      collapseBtnClass: 'el-icon-s-fold',
+      isCollapse_Header:this.isCollapse,
+    }
+  },
+  props:['isCollapse'],
+  methods:{
+    collapse() {  // 点击收缩按钮触发
+<<<<<<< HEAD
+      this.isCollapse_Header= !this.isCollapse_Header
+=======
+      this.isCollapse_Header = !this.isCollapse_Header
+>>>>>>> 963f2ef91c127a140d986ca8d509bd3170206a78
+      if (this.isCollapse_Header) {  // 收缩
+        this.collapseBtnClass = 'el-icon-s-unfold'
+        this.$bus.$emit('show',{isCollapse:this.isCollapse_Header,sideWidth:64,logoTextShow:false})
+      } else {   // 展开
+        this.collapseBtnClass = 'el-icon-s-fold'
+        this.$bus.$emit('show',{isCollapse:this.isCollapse_Header,sideWidth:200,logoTextShow:true})
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/src/components/TypeNav.vue
@@ -0,0 +1,127 @@
+<template>
+  <el-aside :width="sideWidth + 'px'" style="background-color: rgb(238, 241, 246); overflow-x: hidden; box-shadow: 2px 0 6px rgb(0 21 41 / 35%);">
+    <el-menu style="min-height:100%; overflow-x: hidden"
+             background-color="rgb(48, 65, 86)"
+             text-color="#fff"
+             active-text-color="#ffd04b"
+             :collapse-transition="false"
+<<<<<<< HEAD
+             :collapse="isCollapse_TypeNav"
+             router
+=======
+             :collapse="isCollapse_Type"
+>>>>>>> 963f2ef91c127a140d986ca8d509bd3170206a78
+    >
+      <div style="height: 60px; line-height: 60px; text-align: center">
+        <img src="../assets/logo.png" alt="" style="width: 20px; position: relative; top: 5px; margin-right: 5px">
+        <b style="color: white" v-show="logoTextShow">后台管理系统</b>
+      </div>
+<<<<<<< HEAD
+      <el-menu-item index="/">
+          <i class="el-icon-s-home"></i>
+          <span slot="title">主页</span>
+      </el-menu-item>
+      <el-submenu index="">
+=======
+      <el-menu-item index="1-1">
+        <template slot="title">
+          <i class="el-icon-s-home"></i>
+          <span slot="title">主页</span>
+        </template>
+      </el-menu-item>
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-menu"></i>
+          <span slot="title">系统管理</span>
+        </template>
+          <el-menu-item index="1-1">
+            <i class="el-icon-s-custom"></i>
+            <span slot="title">用户管理</span>
+          </el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+      </el-submenu>
+      <!-- <el-submenu index="2">
+>>>>>>> 963f2ef91c127a140d986ca8d509bd3170206a78
+        <template slot="title">
+          <i class="el-icon-menu"></i>
+          <span slot="title">系统管理</span>
+        </template>
+<<<<<<< HEAD
+        <el-menu-item index="/user">
+          <i class="el-icon-s-custom"></i>
+          <span slot="title">用户管理</span>
+        </el-menu-item>
+      </el-submenu>
+
+<!--      <el-submenu index="3">-->
+<!--        <template slot="title">-->
+<!--          <i class="el-icon-setting"></i>-->
+<!--          <span slot="title">导航三</span>-->
+<!--        </template>-->
+<!--        <el-menu-item-group>-->
+<!--          <template slot="title">分组一</template>-->
+<!--          <el-menu-item index="3-1">选项1</el-menu-item>-->
+<!--          <el-menu-item index="3-2">选项2</el-menu-item>-->
+<!--        </el-menu-item-group>-->
+<!--        <el-menu-item-group title="分组2">-->
+<!--          <el-menu-item index="3-3">选项3</el-menu-item>-->
+<!--        </el-menu-item-group>-->
+<!--        <el-submenu index="3-4">-->
+<!--          <template slot="title">选项4</template>-->
+<!--          <el-menu-item index="3-4-1">选项4-1</el-menu-item>-->
+<!--        </el-submenu>-->
+<!--      </el-submenu>-->
+=======
+        <el-menu-item-group>
+          <template slot="title">分组一</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="2-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="2-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+        </el-submenu>
+      </el-submenu> -->
+>>>>>>> 963f2ef91c127a140d986ca8d509bd3170206a78
+    </el-menu>
+  </el-aside>
+</template>
+
+<script>
+export default {
+  name: "TypeNav",
+  data(){
+    return {
+      sideWidth: 200,
+      logoTextShow: true,
+<<<<<<< HEAD
+      isCollapse_TypeNav: this.isCollapse
+=======
+      isCollapse_Type: this.isCollapse,
+>>>>>>> 963f2ef91c127a140d986ca8d509bd3170206a78
+    }
+  },
+  props:['isCollapse'],
+  mounted() {
+    this.$bus.$on('show',(data) =>{
+<<<<<<< HEAD
+      this.isCollapse_TypeNav = data.isCollapse
+=======
+      this.isCollapse_Type = data.isCollapse
+>>>>>>> 963f2ef91c127a140d986ca8d509bd3170206a78
+      this.sideWidth = data.sideWidth
+      this.logoTextShow = data.logoTextShow
+    })
+  },
+  beforeDestroy() {
+    this.$bus.$off('show')
+  },
+}
+</script>
+
+<style scoped>
+
+</style>

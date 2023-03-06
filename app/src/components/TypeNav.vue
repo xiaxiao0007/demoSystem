963f2ef91c127a140d986ca8d509bd3170206a78
Xia <1562232027@qq.com>
2023-03-06 12:21:40 +0800

    update typeNav setting

--- app/src/components/TypeNav.vue
+++ app/src/components/TypeNav.vue
@@ -11,25 +11,24 @@
         <img src="../assets/logo.png" alt="" style="width: 20px; position: relative; top: 5px; margin-right: 5px">
         <b style="color: white" v-show="logoTextShow">后台管理系统</b>
       </div>
+      <el-menu-item index="1-1">
+        <template slot="title">
+          <i class="el-icon-s-home"></i>
+          <span slot="title">主页</span>
+        </template>
+      </el-menu-item>
       <el-submenu index="1">
         <template slot="title">
-          <i class="el-icon-message"></i>
-          <span slot="title">导航一</span>
+          <i class="el-icon-menu"></i>
+          <span slot="title">系统管理</span>
         </template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-1">
+            <i class="el-icon-s-custom"></i>
+            <span slot="title">用户管理</span>
+          </el-menu-item>
           <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-        </el-submenu>
       </el-submenu>
-      <el-submenu index="2">
+      <!-- <el-submenu index="2">
         <template slot="title">
           <i class="el-icon-menu"></i>
           <span slot="title">导航二</span>
@@ -46,25 +45,7 @@
           <template slot="title">选项4</template>
           <el-menu-item index="2-4-1">选项4-1</el-menu-item>
         </el-submenu>
-      </el-submenu>
-      <el-submenu index="3">
-        <template slot="title">
-          <i class="el-icon-setting"></i>
-          <span slot="title">导航三</span>
-        </template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="3-1">选项1</el-menu-item>
-          <el-menu-item index="3-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="3-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="3-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
+      </el-submenu> -->
     </el-menu>
   </el-aside>
 </template>
